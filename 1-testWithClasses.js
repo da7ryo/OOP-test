4ce48c37c272d9e87c18bdbbebab16cabfe04abc
Dario Milanović <da7ryo@gmail.com>
2025-11-30 10:52:55 +0100

    Day 3

--- 1-testWithClasses.js
+++ 1-testWithClasses.js
@@ -61,33 +61,28 @@ class ITStudent extends Student {
   }
 }
 
-const person1 = new Person("Dario", "Milanovic", 1992);
-const person2 = new Person("Ivan", "Pivic", 1985);
+class School {
+  constructor(name) {
+    this.name = name;
+    this.people = [];
+  }
 
-console.log(person1);
-console.log(person2);
-console.log(person1.calcAge());
-person2.greet();
-Person.describe();
-person1.greet();
-console.log(person2.calcAge());
+  addPerson(per) {
+    this.people.push(per);
+  }
 
-const student1 = new Student("Marija", "Milanovic", 1997, 15488);
-const student2 = new Student("Jasmin", "Isakovic", 1990, 12547);
-const student3 = new Student("Ivica", "Jurcevic", 1978, 21478);
-const student4 = new Student("Danijela", "Skelo", 2001, 32546);
-const student5 = new ITStudent("Ivan", "Ivic", 2000, 123);
+  listStudents() {
+    return this.people.filter((p) => p instanceof Student);
+  }
 
-const students = [student1, student2, student3, student4];
+  listTeachers() {
+    return this.people.filter((p) => p instanceof Teacher);
+  }
 
-student1.greet();
-student2.calcAge();
-console.log(student1);
-console.log(students.map((item) => `${item.firstName} ${item.lastName}`));
-console.log(students.filter((item) => 2025 - item.birthYear > 18));
-student5.learnLanguage("JavaScript");
-student5.learnLanguage("Python");
-console.log(student5.programmingLanguages);
+  findPerson(name) {
+    return this.people.find((p) => p.firstName === name);
+  }
+}
 
 function processPeople(arr, callback) {
   for (let i = 0; i < arr.length; i++) {
@@ -102,12 +97,66 @@ function anounce(currentPerson) {
   );
 }
 
-processPeople(students, anounce);
-
 function rewardPerson(currentPerson) {
   if (currentPerson.calcAge() > 18) {
     console.log("Recives a gift");
   }
 }
 
+// Persons
+
+const person1 = new Person("Dario", "Milanovic", 1992);
+const person2 = new Person("Ivan", "Pivic", 1985);
+
+console.log(person1);
+console.log(person2);
+console.log(person1.calcAge());
+person2.greet();
+Person.describe();
+person1.greet();
+console.log(person2.calcAge());
+
+// Students
+
+const student1 = new Student("Marija", "Milanovic", 1997, 15488);
+const student2 = new Student("Jasmin", "Isakovic", 1990, 12547);
+const student3 = new Student("Ivica", "Jurcevic", 1978, 21478);
+const student4 = new Student("Danijela", "Skelo", 2001, 32546);
+const student5 = new ITStudent("Ivan", "Ivic", 2000, 123);
+const students = [student1, student2, student3, student4];
+
+// Teachers
+
+const teacher1 = new Teacher("Matija", "Lukic", 1980, "History", 10);
+const teacher2 = new Teacher("Petra", "Previsic", 1975, "Art", 7);
+const teacher3 = new Teacher("Martina", "Boskovic", 1978, "Math", 15);
+
+student1.greet();
+student2.calcAge();
+console.log(student1);
+console.log(students.map((item) => `${item.firstName} ${item.lastName}`));
+console.log(students.filter((item) => 2025 - item.birthYear > 18));
+student5.learnLanguage("JavaScript");
+student5.learnLanguage("Python");
+console.log(student5.programmingLanguages);
+students.push(student5);
+
+processPeople(students, anounce);
+
 processPeople(students, rewardPerson);
+
+const school = new School("Gymnasium");
+
+school.addPerson(student1);
+school.addPerson(student2);
+school.addPerson(student3);
+school.addPerson(student4);
+school.addPerson(student5);
+school.addPerson(person1);
+school.addPerson(person2);
+school.addPerson(teacher1);
+school.addPerson(teacher2);
+school.addPerson(teacher3);
+console.log(school);
+console.log(school.listStudents());
+console.log(school.listTeachers());
